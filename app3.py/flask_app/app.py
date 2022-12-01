0b13e11968cb52a3ea7974b29e905745e0eaf5cf
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-12-01 23:14:08 +0900

    修正
    修正

--- app3.py/flask_app/app.py
+++ app3.py/flask_app/app.py
@@ -1,13 +1,22 @@
+coding: utf-8
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html' , ¥
-    title="Index  with  Jinja", ¥
+  return render_template('index.html' , 
+    title="Index  with  Jinja", 
     message="これはJinjaテンプレートの利用例です。!")
 
 if __name__=='__main__':
   app.debug = True
   app.run(host='localhost')
+
+
+@app.route('/<id>/<password>')
+def index2(id,  password):
+  msg = 'id: %s,  password:%s ' %(id,password)
+  return  render_template('index.html'  ¥
+    title="Index  with  Jinja", ¥
+    message=msg)
