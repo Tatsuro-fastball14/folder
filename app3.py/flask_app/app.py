a77796a3fc390bef030dac43facd4d244ff24713
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-12-11 19:43:22 +0900

    修正
    修正

--- app3.py/flask_app/app.py
+++ app3.py/flask_app/app.py
@@ -8,15 +8,15 @@ def index():
   return render_template('index.html' , 
     title="Index  with  Jinja", 
     message="これはJinjaテンプレートの利用例です。!")
+@app.route('/<id>/<password>')
+def index2(id,  password):
+  msg = f'id:{id}, pass:{password}'
+  return  render_template('index.html',title="Index with Jinja", message=msg)
+
 
 if __name__=='__main__':
   app.debug = True
   app.run(host='localhost')
 
-
-@app.route('/<id>/<password>')
-def index2(id,  password):
-  msg = 'id:taro ,  password:yamada  '(id,password)
-  return  render_template('index.html',title="Index with Jinja", message=msg)
 
 
