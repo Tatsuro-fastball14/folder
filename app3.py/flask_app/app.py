227e3579d07a159c7d4f5ac2f039f3ff864a1275
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-12-12 23:23:42 +0900

    フォームを受け取り処理する
    フォームを受け取り処理する

--- app3.py/flask_app/app.py
+++ app3.py/flask_app/app.py
@@ -1,5 +1,8 @@
 # -- coding: utf-8 --
-from flask import Flask, render_template
+from  flask import  Flask,  render_template
+from  flask import  Flask,  render_template,  url_for
+from  flask import  Flask,  render_template,  request
+
 
 app = Flask(__name__)
 
@@ -12,6 +15,15 @@ def index():
 def index2(id,  password):
   msg = f'id:{id}, pass:{password}'
   return  render_template('index.html',title="Index with Jinja", message=msg)
+
+@app.route('/', methods=['GET'])
+def index():
+    return  render_template('index.html',title="Form  sample",  message="お名前は？" )
+
+@app.route('/', methods=['post'])
+def form():
+    Field = request.form['field']
+    return  render_template('index.html', title="Form sample",  message="こんにちは、さん!" field)
 
 
 
