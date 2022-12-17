079bce57e2b8a5fe1c7f42a513f533a2e2ed0a0e
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-12-18 05:13:31 +0900

    修正
    修正

--- app3.py/flask_app/app.py
+++ app3.py/flask_app/app.py
@@ -6,15 +6,7 @@ from  flask import  Flask,  render_template,  request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('index.html' , 
-    title="Index  with  Jinja", 
-    message="これはJinjaテンプレートの利用例です。!")
-@app.route('/<id>/<password>')
-def index2(id,  password):
-  msg = f'id:{id}, pass:{password}'
-  return  render_template('index.html',title="Index with Jinja", message=msg)
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -22,7 +14,7 @@ def index():
 
 @app.route('/', methods=['post'])
 def form():
-    Field = request.form['field']
+    field = request.form['field']
     return  render_template('index.html', title="Form sample",  message="こんにちは、%さん!" % field)
 
 
