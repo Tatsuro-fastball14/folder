7d780b2d3073c700272c0e570ea682c04a5eeeef
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2023-02-07 07:14:53 +0900

    if文を使った変数の値による条件分岐
    if文を使った変数の値による条件分岐

--- app3.py/flask_app/app.py
+++ app3.py/flask_app/app.py
@@ -1,40 +1,25 @@
-# -- coding: utf-8 --
-from  flask import  Flask,  render_template
-from  flask import  Flask,  render_template,  url_for
-from  flask import  Flask,  render_template,  request
+from flask  import  Flask, render_template
+
+app =Flask(__name__)
+
+@app.route('/')
+def test_jinja():
+    return  render_template('test_jinja2.html',name="taro")
 
 
 
 
-app = Flask(__name__)
+@app.route('/test_ifa')
+def if_testa(name=None):
+    name='Jiro'
+    return render_template('test_if/html',name=name)
 
-
-
-@app.route('/', methods=['GET'])
-def index():
-    data =['one','Two','Three']
-    person = {'name':'Taro','mail':'taro@yamada'}
-    return render_template('layout.html',title='Template sample',message='これは、サンプルのページです.',data=data,person= person)
-    
-
-@app.route('/', methods=['post'])
-def form():
-    ck  =   request.form.get('check')
-    rd  =   request.form.get('radio')
-    sel =   request.form.getlist('sel')
-    return  render_template('layout.html',   title="Form sample",    message=[ck,rd,sel])
+@app.route('/test_ifb')
+def if_testb(name=None):
+    name='sumijiro'
+    return  render_template('test_if.html',name=name)
 
 
 
 
-
-    field = request.form['field']
-    return  render_template('index.html', title="Form sample",  message="こんにちは、%s さん!" % field)
-
-
-if __name__=='__main__':
-  app.debug = True
-  app.run(host='localhost')
-
-
-
+app.run(debug=True)
