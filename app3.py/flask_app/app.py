8ac3e6101ab4ec926c858efc84e77d94dce8666b
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2023-02-11 06:15:45 +0900

    修正
    修正

--- app3.py/flask_app/app.py
+++ app3.py/flask_app/app.py
@@ -11,13 +11,18 @@ def test_jinja():
 
 @app.route('/test_ifa')
 def if_testa(name=None):
-    name='Jiro'
-    return render_template('test_if/html',name=name)
+    name='jiro'
+    return render_template('test_if.html',name=name)
 
 @app.route('/test_ifb')
 def if_testb(name=None):
     name='sumijiro'
     return  render_template('test_if.html',name=name)
+
+@app.route('/test_for')
+def test_for():
+    brothers=["太郎","次郎","三郎"]
+    return render_template('test_for.html',users=brothers)
 
 
 
