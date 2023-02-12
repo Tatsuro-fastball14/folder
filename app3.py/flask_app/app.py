00080c923a4dd23df6a6aaa3dc1b2d7e57fccde3
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2023-02-12 14:02:41 +0900

    修正
    修正

--- app3.py/flask_app/app.py
+++ app3.py/flask_app/app.py
@@ -20,13 +20,33 @@ def receive_get():
     if len(name) == 0:
         return "名前が未入力です"
     else:
-         return 'あなたが入力した名前は' + str(name) + "です"
+        return 'あなたが入力した名前は' + str(name) + "です"
 
-@app.route('/')
-def test_jinja():
-    return  render_template('test_jinja2.html',name="taro")
+# post処理の入力フォームを表示
+@app.route("/request_post", methods=["GET"])
+def post_sample():
+    return render_template('send_post.html')
 
+# postでの入力情報処理
+@app.route("/request_post", methods=["POST"])
+def post_action():
+    if "gender" in request.form.keys():
+        gender = request.form["gender"]
+        if gender == "男":
+            sex = '男性'
+        elif gender == "女":
+            sex = "女性"
+    else:
+        sex = '性別不明'
 
+    if 'age' in request.form.keys():
+        age_range = request.form['age']
+    else:
+        age_range = '年齢不詳'
+    return f'あなたは{sex}で{age_range}です。'
+
+# テスト環境起動
+app.run(debug=True)
 
 
 
