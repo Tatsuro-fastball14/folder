d79b26fbe4d558cdc393124da12853de1157da39
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2023-02-12 15:16:20 +0900

    修正
    修正

--- app3.py/flask_app/app.py
+++ app3.py/flask_app/app.py
@@ -1,56 +1,34 @@
-from flask import Flask, render_template
-from flask import request
-
+from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
+
 
 # トップ画面
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# get処理の入力フォームを表示
-@app.route("/request_get")
-def get():
-    return render_template('send_get.html')
 
-# getでの入力情報処理
-@app.route("/receive_get", methods=["GET"])
-def receive_get():
-    name = request.args["my_name"]
-    if len(name) == 0:
-        return "名前が未入力です"
-    else:
-        return 'あなたが入力した名前は' + str(name) + "です"
-
-# post処理の入力フォームを表示
-@app.route("/request_post", methods=["GET"])
-def post_sample():
-    return render_template('send_post.html')
-
-# postでの入力情報処理
-@app.route("/request_post", methods=["POST"])
-def post_action():
-    if "gender" in request.form.keys():
-        gender = request.form["gender"]
-        if gender == "男":
-            sex = '男性'
-        elif gender == "女":
-            sex = "女性"
-    else:
-        sex = '性別不明'
-
-    if 'age' in request.form.keys():
-        age_range = request.form['age']
-    else:
-        age_range = '年齢不詳'
-    return f'あなたは{sex}で{age_range}です。'
-
-# テスト環境起動
-app.run(debug=True)
+# 社員一覧
+@app.route("/members")
+def user_url():
+    lists = [[1, '太郎'], [2, '次郎'], [3, '三郎']]
+    return render_template("member.html", lists=lists)
 
 
+# 社員詳細情報
+@app.route("/member_detail/<int:id_>/<name>")
+def detail(id_, name):
+    lists = [['太郎', 24, 'サッカー'], ['次郎', 45, '野球'], ['三郎', 67, 'バスケ']]
+    index = id_-1
+    data = lists[index]
+    return render_template("detail.html", data=data, name=name)
 
 
+# トップ画面にリダイレクト
+@app.route('/redirects')
+def redirects():
+    return redirect(url_for('index'))
 
 
 app.run(debug=True)
+
